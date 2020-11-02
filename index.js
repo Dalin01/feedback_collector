646a51c595243f065942cb10856610c4e375a559
Darlington <somtodarlington@yahoo.com>
2020-11-02 20:27:46 +0100

    setting up Heroku

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send({hi: 'Hello World'});
 })
 
 app.listen(PORT, () => {
-  console.log(`App listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`);
 })
